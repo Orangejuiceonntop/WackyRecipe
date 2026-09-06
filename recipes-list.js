1a4d25040e2827d1963caf94f960c75e80ddaeab
orangejuiceontop <susselkikijimenez@gmail.com>
2026-09-06 11:20:13 -0500

    Create an array for the recipes

---
+++ recipes-list.js
@@ -0,0 +1,21 @@
+let allRecipes = [
+    {
+        name:"Peeping Mushroom Pasta",
+        img:"",
+        ingredients:["2 tablespoons butter", "1/4 cup all purpose flour", "2 cups lukewarm milk",
+            "2 cups lukewarm milk", "1/4 onion", "1 whole clove", "1 bay leaf", "9 ounces rigatoni pasta", "Pinch of freshly ground nutmeg", "Sea salt and white pepper",
+            "2 tablespoons whole-grain mustard", "3 1/2 ounces Gruyère, parmesan or other hard cheese", "6 ounces mixed enoki or shimeji mushrooms", "Finely chopped fresh parsley for serving"],
+        instructions:["Melt the butter in a medium frying pan over medium heat. Add the flour and mix to form a paste, cooking it for 2 minutes. Remove from the heat and let cool for 2 minutes, then gradually add the milk, whisking continuously.",
+            "Place the pan back over medium heat; add the onion, clove, and bay leaf; and simmer gently for 10 minutes, whisking frequently. If the sauce becomes too thick, whisk in a little more milk 1 tablespoon at a time until saucy.",
+            "Preheat the oven to 350°F.", "Bring a stockpot of salted water to a boil. Put the pasta in the water and cook for 2 minutes less than the package instructions say.", "Finish the sauce by removing the onion, clove, and bay leaf, then adding the nutmeg and seasoning with salt and white pepper. Stir in the mustard and half the cheese.",
+            "Drain the pasta and arrange the rigatoni pieces upright tightly in four ovenproof dishes; they will look a bit like honeycomb. Pour the sauce over the pasta. Tap the base of the baking dishes to allow the sauce to get between the holes, spooning more on if necessary. Place the mushroom stalks into the rigatoni holes, leaving the caps poking out. Sprinkle with the remaining cheese.",
+            "Bake for 20 to 25 minutes, or until the cheese is golden and bubbling. Serve with a sprinkle of finely chopped parsley on top."],
+    },
+    {
+        name:"Chorizo and Cheese Grilled Stuffed Bell Peppers",
+        img:"",
+        ingredients:[],
+        instructions:[],
+    }
+    
+]
