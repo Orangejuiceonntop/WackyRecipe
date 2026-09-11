22c82f9b7aae5d5e40dd1e0cfda68d097109ff42
orangejuiceontop <susselkikijimenez@gmail.com>
2026-09-10 20:12:55 -0500

    Hide main container when expand button is pressed to show recipe container

--- script.js
+++ script.js
@@ -1,3 +1,33 @@
-let randomNum = Math.floor(Math.random() * 12) + 1;
+//generate random number
+let randomNum = Math.floor(Math.random() * 2) + 1;
 
 console.log(randomNum);
+
+//linking the generated number to the array thingy
+let recipeIndex = randomNum;
+
+window.addEventListener("load", ()=>{
+    loadRecipes(recipeIndex);
+})
+
+function loadRecipes(indexNumb) {
+    //fill this later
+}
+
+let rollButton = document.getElementById("rollButton");
+let rerollButton = document.getElementById("rerollButton");
+let expandButton = document.getElementById("expandButton");
+let mainContainer = document.getElementById("mainContainer");
+let sideContainer = document.getElementById("sideContainer");
+
+
+rollButton.addEventListener("click", event => {
+    rollButton.style.display = "none";
+    rerollButton.style.display = "flex";
+    expandButton.style.display = "flex";
+})
+
+expandButton.addEventListener("click", event => {
+    mainContainer.style.display = "none";
+    sideContainer.style.display = "flex";
+})
