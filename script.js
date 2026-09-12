a5f9d44bce3c15cdfaab4bfba62390dcdcd51f5d
orangejuiceontop <susselkikijimenez@gmail.com>
2026-09-11 21:02:35 -0500

    Make the second reroll button work

--- script.js
+++ script.js
@@ -19,15 +19,26 @@ let rerollButton = document.getElementById("rerollButton");
 let expandButton = document.getElementById("expandButton");
 let mainContainer = document.getElementById("mainContainer");
 let sideContainer = document.getElementById("sideContainer");
+let recipeContainer = document.getElementById("recipeContainer");
+let rerollTwo = document.getElementById("rerollTwo");
+let rollName = document.getElementById("rollName")
 
 
 rollButton.addEventListener("click", event => {
     rollButton.style.display = "none";
     rerollButton.style.display = "flex";
     expandButton.style.display = "flex";
+    rollName.style.display = "block";
 })
 
 expandButton.addEventListener("click", event => {
     mainContainer.style.display = "none";
     sideContainer.style.display = "flex";
+    recipeContainer.style.display = "flex";
+})
+
+rerollTwo.addEventListener("click", event => {
+    mainContainer.style.display = "flex";
+    sideContainer.style.display = "none";
+    recipeContainer.style.display = "none";
 })
