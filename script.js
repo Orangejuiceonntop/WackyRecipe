d5222356ff149a9e193ac8bedc6a6eec2fdf23fb
orangejuiceontop <susselkikijimenez@gmail.com>
2026-09-15 23:51:34 -0500

    Make ingredients and instructions appear in the recipe container

--- script.js
+++ script.js
@@ -6,15 +6,17 @@ console.log(randomNum);
 //linking the generated number to the array thingy
 let recipeIndex = randomNum;
 
-window.addEventListener("load", ()=>{
-    loadRecipes(recipeIndex);
-})
-
 function loadRecipes(indexNumb) {
     rollName.innerText = allRecipes[indexNumb - 1].name;
     recipeName.innerText = allRecipes[indexNumb - 1].name;
     recipeImage.src = `images/${allRecipes[indexNumb - 1].img}.jpg`;
     recipeImageTwo.src = `images/${allRecipes[indexNumb - 1].img}.jpg`;
+    ingredients.innerText = allRecipes[indexNumb - 1].ingredients;
+    instructions.innerText = allRecipes[indexNumb - 1].instructions;
+}
+
+function rollRecipe() {
+    window.location.reload()
 }
 
 let rollButton = document.getElementById("rollButton");
@@ -30,12 +32,16 @@ let recipeName = document.getElementById("recipeName");
 let recipeImage = mainContainer.querySelector(".imgBox img");
 let recipeImageTwo = sideContainer.querySelector(".imgBox img");
 
+let instructions = document.getElementById("instructions");
+let ingredients = document.getElementById("ingredients");
 
+//make buttons work
 rollButton.addEventListener("click", event => {
     rollButton.style.display = "none";
     rerollButton.style.display = "flex";
     expandButton.style.display = "flex";
     rollName.style.display = "block";
+    loadRecipes(recipeIndex);
 })
 
 expandButton.addEventListener("click", event => {
@@ -44,8 +50,13 @@ expandButton.addEventListener("click", event => {
     recipeContainer.style.display = "flex";
 })
 
+rerollButton.addEventListener("click", event => {
+    rollRecipe()
+})
+
 rerollTwo.addEventListener("click", event => {
     mainContainer.style.display = "flex";
     sideContainer.style.display = "none";
     recipeContainer.style.display = "none";
+    rollRecipe();
 })
